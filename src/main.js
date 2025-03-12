c50b0dce8979d7ca7fb6c26669756ff4a1ce47f7
tranvanvu <b2203538@example.com>
2025-03-12 11:21:26 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import router from "./router"; 
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); 
+app.mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(), 
+  routes,
+});
+
+export default router;
